c98f0d11b67bdcb9bcbb473cde73ca74b7047529
snorbit <jedfleming2010@gmail.com>
2026-03-19 06:57:04 +1100

    up

---
+++ common/src/main/java/com/snorbitzz/astralskies/Constants.java
@@ -0,0 +1,11 @@
+package com.snorbitzz.astralskies;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "astral_skies";
+	public static final String MOD_NAME = "astral_skies";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ neoforge/src/main/java/com/snorbitzz/astralskies/platform/NeoForgePlatformHelper.java
@@ -0,0 +1,26 @@
+package com.snorbitzz.astralskies.platform;
+
+import com.snorbitzz.astralskies.platform.services.IPlatformHelper;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
+
+public class NeoForgePlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+
+        return "NeoForge";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return ModList.get().isLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return !FMLLoader.isProduction();
+    }
+}
